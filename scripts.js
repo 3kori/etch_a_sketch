254644f5aa1fa3eb1495bb60614310478d6702d2
3kori <127242906+3kori@users.noreply.github.com>
2025-11-02 22:21:17 +0300

    Create a grid layout

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+
+    for (let j = 0; j < 16; j++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        column.appendChild(row);
+    }
+    container.appendChild(column);
+}
